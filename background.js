c54232ea5508b8321aee4ef561f0ceda58fc51d9
Ngô͘ Hê-bí <shiami@users.noreply.github.com>
2018-11-20 14:27:44 +0800

    Kiàn-gī siu-kái jī-sû
    Kiàn-gī kái chò án-ne, tùi chho͘-ha̍k ê lâng ē khah kán-tan, ló͘-la̍t.

--- background.js
+++ background.js
@@ -1,52 +1,52 @@
 chrome.contextMenus.removeAll(function () {
     chrome.contextMenus.create({
         id: 'poj-equals',
-        title: 'Pe̍hōejī (Kāng 款 ê)',
+        title: 'Pe̍h-ōe-jī (Kāng款--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'poj-contains',
-        title: 'Pe̍hōejī (相關 ê)',
+        title: 'Pe̍h-ōe-jī (相關--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hoalo-equals',
-        title: '教育部羅馬字 (Kāng 款 ê)',
+        title: '教育部羅馬字 (Kāng款--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hoalo-contains',
-        title: '教育部羅馬字 (相關 ê)',
+        title: '教育部羅馬字 (相關--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hanlo-equals',
-        title: '漢羅併用 (Kāng 款 ê)',
+        title: '漢字/漢羅合用 (Kāng款--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hanlo-contains',
-        title: '漢羅併用 (相關 ê)',
+        title: '漢字/漢羅合用 (相關--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hoabun-equals',
-        title: '對應華語 (Kāng 款 ê)',
+        title: '對應華語 (Kāng款--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'hoabun-contains',
-        title: '對應華語 (相關 ê)',
+        title: '對應華語 (相關--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'english-equals',
-        title: '對應英文 (Kāng 款 ê)',
+        title: '對應英文 (Kāng款--ê)',
         contexts: ['selection']
     })
     chrome.contextMenus.create({
         id: 'english-contains',
-        title: '對應英文 (相關 ê)',
+        title: '對應英文 (相關--ê)',
         contexts: ['selection']
     })
 })
